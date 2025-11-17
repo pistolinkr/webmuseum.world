9309e816a96025bef220855ddcd026e63f8b3324
Pistol™ <pistolinkr@icloud.com>
2025-11-17 09:40:36 +0900

    Fix header to show Sign In/Sign Up buttons on all pages

--- components/landing/LandingHeaderWrapper.tsx
+++ components/landing/LandingHeaderWrapper.tsx
@@ -1,16 +1,9 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import LandingHeader from './LandingHeader';
 
 export default function LandingHeaderWrapper() {
-  const pathname = usePathname();
-  const isLandingPage = pathname === '/';
-
-  if (!isLandingPage) {
-    return null;
-  }
-
+  // Always show header on all pages
   return <LandingHeader />;
 }
 
